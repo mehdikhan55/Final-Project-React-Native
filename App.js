6caaca9a264927f5f2400557d1a66366801cdf00
Mehdi Ali Khan <130605561+mehdikhan55@users.noreply.github.com>
2023-07-31 21:53:19 +0500

    uploaded project files

---
+++ App.js
@@ -0,0 +1,117 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useEffect, useMemo, useReducer, useCallback } from "react";
+import { Alert } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { auth_context } from "./contexts/AuthContext";
+import Onboarding from "./screens/Onboarding";
+import Profile from "./screens/Profile";
+import splash_screen from "./screens/SplashScreen";
+import Home from "./screens/Home";
+import { StatusBar } from "expo-status-bar";
+
+const Stack = createNativeStackNavigator();
+
+export default function App({ navigation }) {
+  const [state, dispatch] = useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case "onboard":
+          return {
+            ...prevState,
+            isLoading: false,
+            isOnboardingCompleted: action.isOnboardingCompleted,
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isOnboardingCompleted: false,
+    }
+  );
+
+  useEffect(() => {
+    (async () => {
+      let profileData = [];
+      try {
+        const getProfile = await AsyncStorage.getItem("profile");
+        if (getProfile !== null) {
+          profileData = getProfile;
+        }
+      } catch (e) {
+        console.error(e);
+      } finally {
+        if (Object.keys(profileData).length != 0) {
+          dispatch({ type: "onboard", isOnboardingCompleted: true });
+        } else {
+          dispatch({ type: "onboard", isOnboardingCompleted: false });
+        }
+      }
+    })();
+  }, []);
+
+  const auth_context = useMemo(
+    () => ({
+      onboard: async data => {
+        try {
+          const jsonValue = JSON.stringify(data);
+          await AsyncStorage.setItem("profile", jsonValue);
+        } catch (e) {
+          console.error(e);
+        }
+
+        dispatch({ type: "onboard", isOnboardingCompleted: true });
+      },
+      update: async data => {
+        try {
+          const jsonValue = JSON.stringify(data);
+          await AsyncStorage.setItem("profile", jsonValue);
+        } catch (e) {
+          console.error(e);
+        }
+
+        Alert.alert("Success", "Successfully saved changes!");
+      },
+      logout: async () => {
+        try {
+          await AsyncStorage.clear();
+        } catch (e) {
+          console.error(e);
+        }
+
+        dispatch({ type: "onboard", isOnboardingCompleted: false });
+      },
+    }),
+    []
+  );
+
+  if (state.isLoading) {
+    return <splash_screen />;
+  }
+
+  return (
+    <auth_context.Provider value={auth_context}>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          {state.isOnboardingCompleted ? (
+            <>
+              <Stack.Screen
+                name="Home"
+                component={Home}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen name="Profile" component={Profile} />
+            </>
+          ) : (
+            <Stack.Screen
+              name="Onboarding"
+              component={Onboarding}
+              options={{ headerShown: false }}
+            />
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </auth_context.Provider>
+  );
+}
